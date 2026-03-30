ce28746e5897c300cb2c7b2dfbc3659c047e75dd
Carmesí <sguerrerobaron@gmail.com>
2026-03-30 11:02:36 -0500

    30/03/2026

--- app/src/main/java/com/empresa/autocleaner/SettingsActivity.kt
+++ app/src/main/java/com/empresa/autocleaner/SettingsActivity.kt
@@ -275,6 +275,7 @@ fun SettingsScreen() {
     var daysToKeep by remember { mutableFloatStateOf(SettingsManager.getDaysToKeep(context).toFloat()) }
     var frequencyPosition by remember { mutableFloatStateOf(SettingsManager.getExecutionFrequency(context).toFloat()) }
     var showDatePicker by remember { mutableStateOf(false) }
+    var showTimePicker by remember { mutableStateOf(false) }
 
     LaunchedEffect(Unit) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R && !Environment.isExternalStorageManager()) {
@@ -361,22 +362,51 @@ fun SettingsScreen() {
             }
         }
         Spacer(modifier = Modifier.height(20.dp))
-        Column(){
-            Row() {
+        Column(
+            modifier = Modifier
+                .background(color = Color(0xFFFFFFFF), shape = RoundedCornerShape(10.dp))
+                .border(1.dp, Color(0xFF174375), shape = RoundedCornerShape(10.dp))
+        ){
+            Row(
+                modifier = Modifier.width(400.dp)
+            ) {
                 Icon(
                     painter = painterResource(R.drawable.reloj),
                     contentDescription = null,
+                    tint = Color.Unspecified,
+                    modifier = Modifier
+                        .padding(start = 15.dp, top = 8.dp)
+                        .height(50.dp)
+                        .width(50.dp)
                 )
-                Text(text = "Ejecutar limpieza cada ${frequencyPosition.roundToInt()} horas")
+                Text(
+                    text = "Ejecutar limpieza cada ${frequencyPosition.roundToInt()} horas",
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier.padding(top = 20.dp, start = 10.dp)
+                )
             }
         }
         Spacer(modifier = Modifier.height(8.dp))
-        Slider(value = frequencyPosition, onValueChange = { frequencyPosition = it }, onValueChangeFinished = {
+        Button(
+            colors =ButtonDefaults.buttonColors(containerColor = Color(0xFF174375)),
+            modifier = Modifier
+                .padding(bottom = 10.dp, start = 45.dp),
+            onClick = { showTimePicker = true }
+        ){
+            Icon(painter = painterResource(R.drawable.relojcalendario), contentDescription = null)
+            Spacer(modifier = Modifier.width(8.dp))
+            Text("Cambiar frecuencia de ejecución")
+        }
+        if (showTimePicker){
+            DialExample(
+                onConfirm = { frequencyPosition = timePickerState.hour.toFloat()},
+        },
+        /*Slider(value = frequencyPosition, onValueChange = { frequencyPosition = it }, onValueChangeFinished = {
             val hours = frequencyPosition.roundToInt()
             SettingsManager.saveExecutionFrequency(context, hours)
             WorkerScheduler.schedule(context.applicationContext)
             Toast.makeText(context, "Frecuencia guardada: $hours horas.", Toast.LENGTH_SHORT).show()
-        }, valueRange = 1f..24f, steps = 22)
+        }, valueRange = 1f..24f, steps = 22)*/
         Spacer(modifier = Modifier.height(16.dp))
         Text("La tarea de limpieza se ejecutará automáticamente cada ${frequencyPosition.roundToInt()} horas con la configuración guardada.")
         Spacer(modifier = Modifier.height(24.dp))
